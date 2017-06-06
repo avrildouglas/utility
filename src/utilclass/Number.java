b472212e6ce53512e38dfbe725599ae5a38979e5
student <avrildouglas>
2017-06-06 11:10:35 -0400

    fourth commit

--- src/utilclass/Number.java
+++ src/utilclass/Number.java
@@ -1,31 +1,42 @@
 package utilclass;
 
 import java.text.NumberFormat;
+import java.util.Random;
+
 
 public class Number {
 	
 	public static String FormatPercentage(double majority) {
 		NumberFormat percent = NumberFormat.getPercentInstance();
+		percent.setMaximumFractionDigits(2);
 		String majorityString = percent.format(majority);
 		return majorityString;
 	}
 	
-	public static String FormatPercentage(double majority, double twodec) {
-		NumberFormat percentone = NumberFormat.getPercentInstance();
-		percentone.setMaximumFractionDigits(2);
-		String majorityString = percentone.format(majority);
-		NumberFormat percenttwo = NumberFormat.getPercentInstance();
-	//	percenttwo.setMaximumFractionDigits();
-		String twodecString = percenttwo.format(twodec);
-		return majorityString;
-		return twodecString;
-	}
-	
+	public static String[] FormatPercentage(double majority, double twodec) {
+	    String[] percentArray = new String[2];
+		NumberFormat percent = NumberFormat.getPercentInstance();
+		percentArray[0] = percent.format(majority);
+		NumberFormat percentTwo = NumberFormat.getPercentInstance();
+		percentArray[1] = percentTwo.format(twodec);
+	    return percentArray; 
+	}	    
+		
 	public static String FormatCurrency(double price) {
 		NumberFormat currency = NumberFormat.getCurrencyInstance();
+		currency.setMaximumFractionDigits(2);
 		String priceString = currency.format(price);
 		return priceString;
 	}
+	
+	public static String[] FormatCurrency(double price, double pricetwo) {
+	    String[] priceArray = new String[2];
+		NumberFormat currency = NumberFormat.getCurrencyInstance();
+		priceArray[0] = currency.format(price);
+		NumberFormat currencyTwo = NumberFormat.getCurrencyInstance();
+		priceArray[1] = currencyTwo.format(pricetwo);
+	    return priceArray; 
+	}	    
 	
 	public static String FormatDecimal(double miles) {
 		NumberFormat number = NumberFormat.getNumberInstance();
@@ -34,25 +45,41 @@ public class Number {
 		return mileString;
 	}
 	
+	public static String[] FormatDecimal(double miles, double milestwo) {
+	    String[] decimalArray = new String[2];
+		NumberFormat number = NumberFormat.getNumberInstance();
+		decimalArray[0] = number.format(miles);
+		NumberFormat numberTwo = NumberFormat.getNumberInstance();
+		decimalArray[1] = numberTwo.format(milestwo);
+	    return decimalArray; 
+		}	    		
+
 	/**return a random number between two values, A and B (inclusive)
 	see the previous assignment on Random numbers*/
-	public static String FormatRandom(double A, double B) {
-
-		NumberFormat randomnum = NumberFormat.getNumberInstance();
-		String randomnumString = randomnum.format(Math.random() * A + B);
-		return randomnumString;
+	public static int FormatRandom(int A, int B) {
+		Random r = new Random();
+		//NumberFormat randomnum = NumberFormat.;
+		//String randomnumString = r.r
+		int randomnum = 1 + r.nextInt(A) + B;
+		
+				//rformat(Math.random() * A + B);
+		return randomnum;
 	}
 	
 	public static String FormatMax(double numone, double numtwo) {
-		NumberFormat maxval = NumberFormat.getCurrencyInstance();
+		NumberFormat maxval = NumberFormat.getNumberInstance();
 		String maxvalString = maxval.format(Math.max(numone, numtwo));
 		return maxvalString;
 	}
 	
 	public static String FormatMin(double numthree, double numfour) {
-		NumberFormat minval = NumberFormat.getCurrencyInstance();
+		NumberFormat minval = NumberFormat.getNumberInstance();
 		String minvalString = minval.format(Math.min(numthree, numfour));
 		return minvalString;
 	}
+	
 }
 
+	
+	
+	
